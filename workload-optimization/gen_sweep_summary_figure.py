444bc37f4f3656154f3315b063b7a147ee7a052b
Leo Han <leo.xc.han@protonmail.com>
2025-04-21 16:52:39 -0400

    update 2x5 summary figure

--- workload-optimization/gen_sweep_summary_figure.py
+++ workload-optimization/gen_sweep_summary_figure.py
@@ -7,8 +7,33 @@ fair_co2_path = os.environ.get('FAIR_CO2')
 figures_dir = f'{fair_co2_path}/figures'
 data_dir = f'{fair_co2_path}/workload-optimization/results'
 
+# Hardcoded label box positions
+wc_label_pos = [(2.5, 0.88), (1.5, 1.09), (0.5, 0.4), (0.5, 0.5), (0.5, 0.5)]
+sa_label_pos = [(1.3, 0.85), (0.5, 0.6), (0.5, 0.6), (0.5, 0.6), (0.5, 0.6)]
+ddup_label_pos = [(0.75, 0.7), (0.5, 0.8), (0.5, 0.9), (0.5, 0.9), (0.5, 0.9)]
+nn_label_pos = [(0.5, 1.05), (0.5, 0.8), (0.5, 0.9), (0.5, 0.9), (0.5, 0.9)]
+faiss_label_pos = [(2.9, 0.915), (0.7, 0.74), (0.5, 0.85), (0.5, 0.7), (0.5, 0.7)]
+nbody_label_pos = [(0.6, 0.7), (0.5, 0.8), (0.5, 0.8), (0.5, 0.8), (0.5, 0.8)]
+msf_label_pos = [(2.6, 1.2), (1.1, 0.73), (1, 0.88), (0.5, 0.8), (0.5, 1.0)]
+ch_label_pos = [(0.5, 0.8), (0.5, 0.8), (0.5, 0.8), (0.5, 0.8), (0.5, 0.8)]
+bfs_label_pos = [(0.95, 0.6), (1.5, 0.75), (0.5, 0.8), (0.5, 0.8), (0.5, 0.8)]
+spark_label_pos = [(2, 2.3), (1.2, 0.6), (1.5, 1.4), (0.6, 1.4), (0.5, 1.4)]
+
+label_pos = {
+    'wordCounts': wc_label_pos,
+    'suffixArray': sa_label_pos,
+    'removeDuplicates': ddup_label_pos,
+    'nearestNeighbors': nn_label_pos,
+    'faiss': faiss_label_pos,
+    'nBody': nbody_label_pos,
+    'minSpanningForest': msf_label_pos,
+    'convexHull': ch_label_pos,
+    'breadthFirstSearch': bfs_label_pos,
+    'spark': spark_label_pos
+}
+
 def plot_summary_normalized_2x5(workload_list, workload_label_list, grid_ci_list):
-    fig, axs = plt.subplots(2, 5, figsize=(20, 5), sharex=True, sharey=False)
+    fig, axs = plt.subplots(2, 5, figsize=(20, 5.5), sharex=True, sharey=False)
     for i, workload in enumerate(workload_list):
         if workload == 'spark':
             # Read from pickle
@@ -129,7 +154,7 @@ def plot_summary_normalized_2x5(workload_list, workload_label_list, grid_ci_list
         axs[i//5, i%5].plot(grid_ci_list, min_energy[:len(grid_ci_list)], color='blue', linestyle='dotted', linewidth=linewidth)
         axs[i//5, i%5].plot(grid_ci_list, min_embodied[:len(grid_ci_list)], color='orange', linestyle='dashdot', linewidth=linewidth)
         axs[i//5, i%5].plot(grid_ci_list, min_cf[:len(grid_ci_list)], color='green', linestyle='dashed', linewidth=linewidth)
-        fig.legend(['Min Runtime', 'Min Operational', 'Min Embodied', 'Min Carbon Footprint'], loc='upper center', ncol=4, fontsize=16)
+        fig.legend(['Min Runtime', 'Min Operational', 'Min Embodied', 'Min Carbon Footprint'], loc='upper center', ncol=4, fontsize=18)
         region_colors = ['green', 'lightgreen']
         region_offsets = [0, 0.2, -0.1, 0.1, -0.2]
         for k, region in enumerate(regions):
@@ -137,27 +162,36 @@ def plot_summary_normalized_2x5(workload_list, workload_label_list, grid_ci_list
             bbox_props = dict(boxstyle="round,pad=0.3", fc=region_colors[k % 2], ec="black", lw=2, alpha=0.5)
             (ylim_min, ylim_max) = axs[i//5, i%5].get_ylim()
             # Offset boxes so they don't overlap
+            # if workload == 'faiss':
+            #     axs[i//5, i%5].text((region[0] + region[1])/2, ylim_min + (ylim_max - ylim_min)*(0.6 + region_offsets[k%5]), f'{region_index[k]} \n{region_cpu[k]} c\n{region_batch_size[k]} batch', ha="center", va="center", rotation=0, bbox=bbox_props, fontsize=9)
+            # else:
+            #     axs[i//5, i%5].text((region[0] + region[1])/2, ylim_min + (ylim_max - ylim_min)*(0.6 + region_offsets[k%5]), f'{region_cpu[k]} c\n{region_memory[k]} GB', ha="center", va="center", rotation=0, bbox=bbox_props, fontsize=9)
+
             if workload == 'faiss':
-                axs[i//5, i%5].text((region[0] + region[1])/2, ylim_min + (ylim_max - ylim_min)*(0.6 + region_offsets[k%5]), f'{region_index[k]} \n{region_cpu[k]} c\n{region_batch_size[k]} batch', ha="center", va="center", rotation=0, bbox=bbox_props, fontsize=9)
+                if region_index[k] == 'ivf': index_label = 'IVF'
+                elif region_index[k] == 'hnsw': index_label = 'HNSW'
+
+                axs[i//5, i%5].text(region[0] + (region[1] - region[0])*label_pos[workload][k][0], label_pos[workload][k][1], f'{index_label} \n{int(region_cpu[k])} C\n{int(region_batch_size[k])} BS', ha="center", va="center", rotation=0, bbox=bbox_props, fontsize=10)
             else:
-                axs[i//5, i%5].text((region[0] + region[1])/2, ylim_min + (ylim_max - ylim_min)*(0.6 + region_offsets[k%5]), f'{region_cpu[k]} c\n{region_memory[k]} GB', ha="center", va="center", rotation=0, bbox=bbox_props, fontsize=9)
-        axs[i//5, i%5].set_title(workload_label_list[i], fontsize = 16)
+                axs[i//5, i%5].text(region[0] + (region[1] - region[0])*label_pos[workload][k][0], label_pos[workload][k][1], f'{int(region_cpu[k])} C\n{int(region_memory[k])} GB', ha="center", va="center", rotation=0, bbox=bbox_props, fontsize=10)
+        axs[i//5, i%5].set_title(workload_label_list[i], fontsize = 20)
         axs[i//5, i%5].set_xlim(0, 400)
         # Set x-axis ticks
-        axs[i//5, i%5].set_xticks(range(0, 410, 50))
+        axs[i//5, i%5].set_xticks(range(0, 410, 100))
         axs[i//5, 0].set_ylabel(' ')
         axs[1, i%5].set_xlabel(' ')
+        axs[i//5, i%5].tick_params(axis='y', which='major', labelsize=16)
+        axs[i//5, i%5].tick_params(axis='x', which='major', labelsize=16)
 
     # Label the x-axis centered in the whole plot
-    fig.text(0.5, 0.01, 'Grid Carbon Intensity (gCO2eq/kWh)', ha='center', fontsize=16)
+    fig.text(0.5, 0.02, 'Grid Carbon Intensity (gCO2eq/kWh)', ha='center', fontsize=20)
     # Label the y-axis centered in the whole plot
-    fig.text(0.005, 0.5, 'Normalized Total Carbon Footprint', va='center', rotation='vertical', fontsize=16)
+    fig.text(0, 0.5, 'Norm. Carbon Footprint', va='center', rotation='vertical', fontsize=20)
     
     fig.tight_layout()
     # Add more space at the top for the legend
-    fig.subplots_adjust(top=0.83)
+    fig.subplots_adjust(bottom=0.13, top=0.83)
     plt.savefig(f'{figures_dir}/10_summary_normalized_2x5.pdf')
-    plt.show()
     plt.close()
 
 
